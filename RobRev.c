b22fb87743462b84eaaf0e16fc84a12068b11487
Ashish <shinyflame007@gmail.com>
2020-01-15 20:58:09 -0500

    testing the beeper

--- RobRev.c
+++ RobRev.c
@@ -1,9 +1,9 @@
 /*==============================================================================
-	Project: Sumo
-	Version: 3.0				Date: June 15, 2016
-	Target: CHRPmini			Processor: PIC16F886
+    Project: Sumo
+    Version: 3.0				Date: June 15, 2016
+    Target: CHRPmini			Processor: PIC16F886
 
-	A sonar-based Sumo robot.
+    A sonar-based Sumo robot.
 ==============================================================================*/
 
 #include	"xc.h"				// XC compiler general include file
@@ -14,21 +14,21 @@
 #include	"CHRP3.h"		// User-created variables and functions
 
 /*==============================================================================
-	Global variable definitions
+    Global variable definitions
  =============================================================================*/
-unsigned char mode;				// Operating modes (search, attack))
-unsigned char range;			// Target range in cm
-unsigned char target;			// Target acquisition counter
+unsigned char mode; // Operating modes (search, attack))
+unsigned char range; // Target range in cm
+unsigned char target; // Target acquisition counter
 
 /*==============================================================================
-	Program constant definitions
+    Program constant definitions
  =============================================================================*/
 #define	search		0			// Search mode
 #define	attack		1			// Attack mode
 #define	maxRange	120			// Maximum sonar target range in cm
 
 /*==============================================================================
-	Motor direction constant definitions
+    Motor direction constant definitions
  =============================================================================*/
 #define STOP		0b00000000	// Both motors stopped
 #define FWD			0b00000110	// Both motors forward
@@ -41,62 +41,32 @@ unsigned char target;			// Target acquisition counter
 #define	LEFTREV		0b00001000	// Right motor reversed, left motor stopped
 
 /*==============================================================================
-	Sonar range function. Returns target distance in cm, or 0 if error.
+    Sonar range function. Returns target distance in cm, or 0 if error.
  =============================================================================*/
-unsigned char sonar(void)
-{
-//	unsigned char range = 0;
-	
-	while(ECHO == 1);			// Wait until previous transmission has finished
-	__delay_ms(1);				// Add a delay and then
-	TRIG = 1;					// start a new ping
-	__delay_us(20);
-	TRIG = 0;
-	while(ECHO == 0);			// Wait for sonar pulse transmission to finish
-	range = 0;					// Reset range
-
-	while(ECHO == 1)
-	{
-		__delay_us(50);			// Increment distance while waiting for echo
-		range ++;
-		if(range == maxRange)	// Check for over-range and return error
-			return(0);
-	}
-	return(range);				// Return distance to target in cm
-}
 
 /*==============================================================================
-	Main program code
+    Main program code
 ==============================================================================*/
+void beep(unsigned char period, unsigned char cycles)
+{
+	unsigned char i;
+	unsigned char t;
+	for (i = cycles; i != 0; i --)		// number of beeper pin output flips
+	{
+		BEEPER = !BEEPER;				// flip beeper pin and
+		for (t = period; t != 0; t --);	// wait for period to end
+	}
+}
 
 int main(void)
 {
+//	 Initialize I/O and peripherals for PORTB output
 	initPorts();
 
-
-	// Wait for button press and then delay 5s
-
-    // Set starting conditions
-	LED3 = 1;					// Turn the floor LEDs on
-	mode = search;				// Set search mode
-	
-	PORTB = LEFT;				// Turn left
-	
 	while(1)
-	{        
-		while(mode == search)		// Search mode
-		{
-			range = sonar();		// Ping
-			
-			
-			
-		}
+    {
 
-		while(mode == attack)
-		{
-			PORTB = FWD;			// Attack mode
-
-			range = sonar();		// Ping
-		}		
-	}
+	if (S2 == 0)
+			beep(60,200);			// Beep for 200 counts of 60 loop delays
+        }
 }
